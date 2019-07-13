c0fba7f7f8680e79b358a134c9cc0b3ae3b106c0
guoguo <noobwu@126.com>
2019-07-13 09:25:27 +0800

    Initial commit

---
+++ Opserver/Models/Security/EveryonesAnAdminProvider.cs
@@ -0,0 +1,36 @@
+﻿using System.Configuration;
+
+namespace StackExchange.Opserver.Models.Security
+{
+    /// <summary>
+    /// Does this REALLY need an explanation?
+    /// </summary>
+    public class EveryonesAnAdminProvider : SecurityProvider
+    {
+        public override bool IsAdmin => true;
+
+        internal override bool InAdminGroups(ISecurableModule settings) { return true; }
+        public override bool InGroups(string groupNames, string accountName) { return true; }
+        public override bool ValidateUser(string userName, string password) {
+            var adminUsers = ConfigurationManager.AppSettings["AdminUsers"];
+            if (string.IsNullOrEmpty(adminUsers))
+            {
+                return false;
+            }
+            var adminUsersArray = adminUsers.Split(new char[] { ',' }, System.StringSplitOptions.RemoveEmptyEntries);
+            if (adminUsersArray == null || adminUsersArray.Length == 0)
+            {
+                return false;
+            }
+            foreach (var item in adminUsersArray)
+            {
+                string[] tmpUser = item.Split(new char[] { ':' }, System.StringSplitOptions.RemoveEmptyEntries);
+                if (tmpUser != null && tmpUser.Length == 2)
+                {
+                    return userName == tmpUser[0] && password == tmpUser[1];
+                }
+            }
+            return false;
+        }
+    }
+}
